10f9920f55d120a7b4eab03c7930d991c7174150
muh-osman <mr.muhammed.osman@gmail.com>
2026-01-01 19:50:48 +0300

    Bug fixes

--- even-or-odd.js
+++ even-or-odd.js
@@ -1,7 +1,7 @@
+// Even or Odd
 // https://youtu.be/hPHN31gNEMk
 // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe/train/javascript
 
-// Even or Odd
 // Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 // const evenOrOdd = (num) => {
---
+++ multiply.js
@@ -0,0 +1,13 @@
+// Multiply
+// https://youtu.be/Cwtx2NTvfXg
+// https://www.codewars.com/kata/50654ddff44f800200000004/solutions/javascript
+
+// const multiply = (a, b) => {
+//   return a * b;
+// };
+
+// console.log(multiply(7, 2));
+
+const multiply = (a, b) => a * b;
+
+console.log(multiply(7, 2));
